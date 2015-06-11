7ecdde6a9afe9b61734dae55b347c2f9e5a5d93a
Kevin Ingersoll <kingersoll@gmail.com>
2015-06-11 15:09:48 -0700

    Simplify preview URL
    Thanks to https://github.com/rgrove/rawgit/pull/73

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -19,13 +19,13 @@ git add -A
 git commit -m "gostatic $TRAVIS_COMMIT" -m "https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
 git push origin $DEPLOY_BRANCH
 PUSH_EXIT_CODE=$?
-DEPLOY_SHA=$(git rev-parse HEAD)
+DEPLOY_SHA=$(git rev-parse --short HEAD)
 popd
 
 if [ $PUSH_EXIT_CODE == 0 ] && [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     curl -X POST \
          -H "Authorization: Bearer $HIPCHAT_TOKEN" \
          -H "Content-type: application/json" \
-         -d "{\"color\": \"green\", \"message_type\": \"html\", \"message\": \"Successful <a href='https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID'>build</a> of <a href='https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST'>pull request $TRAVIS_PULL_REQUEST</a>. Preview it <a href='https://rawgit.com/iwantmyname/blog/$DEPLOY_SHA/index.html'>here</a>.\"}" \
+         -d "{\"color\": \"green\", \"message_type\": \"html\", \"message\": \"Successful <a href='https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID'>build</a> of <a href='https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST'>pull request $TRAVIS_PULL_REQUEST</a>. Preview it <a href='https://rawgit.com/iwantmyname/blog/$DEPLOY_SHA/'>here</a>.\"}" \
          https://api.hipchat.com/v2/room/$HIPCHAT_ROOM/notification
 fi
